3b6c5206ffe2778aa5e961ff7ac016fe3300e05b
klauddius <klauddius@gmail.com>
2021-05-07 18:03:53 -0300

    Files from Class 3 and 4

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,13 +3,27 @@ print("Bem vindo ao jogo de Adivinhação")
 print('********************************')
 
 numero_secreto = 42
+rodada = 1
+total_tentativas = 3
 
-chute = int(input("Digite o seu número: "))
+while (rodada <= total_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ", chute)
 
-print("Você digitou ", chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")
+    if (acertou):
+        print("Você acertou")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior do que o número secreto")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
+    rodada=rodada+1
+
+print("Fim do Jogo")
